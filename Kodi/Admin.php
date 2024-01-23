1d524b0ba55ae228ac0f8d456f75eccedd2beb14
Dinora Osmani <do39566@ubt-uni.net>
2024-01-23 01:08:14 +0100

    Editimi i Admin.php dhe Admin.html

--- Kodi/Admin.php
+++ Kodi/Admin.php
@@ -1,39 +1,39 @@
 <?php
 $host = "localhost";
-$username = "username"; 
-$password = "password"; 
+$username = "root"; 
+$password = " "; 
 $database = "bakery";
-
 $conn = new mysqli($host, $username, $password, $database);
-
 if ($conn->connect_error) {
-    die("Lidhja me bazën e të dhënave ka deshtuar: " . $conn->connect_error);
+    die("Connection to the database failed: " . $conn->connect_error);
 }
 
 $sql = "SELECT * FROM user";
 $result = $conn->query($sql);
 
-if ($result->num_rows > 0) {
-    echo "<table border='1'>
-            <tr>
-                <th>Name</th>
-                <th>Email</th>
-                <th>Password</th>
-                <th>Role</th>
-            </tr>";
+if ($result) {
+    if ($result->num_rows > 0) {
+        echo "<table border='1'>
+                <tr>
+                    <th>Email</th>
+                    <th>Password</th>
+                    <th>Role</th>
+                </tr>";
 
-    while ($row = $result->fetch_assoc()) {
-        echo "<tr>
-                <td>" . $row["name"] . "</td>
-                <td>" . $row["email"] . "</td>
-                <td>" . $row["password"] . "</td>
-                <td>" . $row["role"] . "</td>
-              </tr>";
+        while ($row = $result->fetch_assoc()) {
+            echo "<tr>
+                    <td>" . $row["email"] . "</td>
+                    <td>" . $row["password"] . "</td>
+                    <td>" . $row["role"] . "</td>
+                  </tr>";
+        }
+
+        echo "</table>";
+    } else {
+        echo "No data found";
     }
-
-    echo "</table>";
 } else {
-    echo "Nuk ka të dhëna të gjetura";
+    echo "Error in the SQL query: " . $conn->error;
 }
 
 $conn->close();
